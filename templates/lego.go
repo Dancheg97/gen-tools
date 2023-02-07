6bb20ff8b1dc8fc43288dc243f869d07a4338eb8
dancheg97 <dangdancheg@gmail.com>
2023-02-07 16:39:49 +0300

    readme updated

--- templates/lego.go
+++ templates/lego.go
@@ -4,4 +4,5 @@ const LegoSh = `go install github.com/go-acme/lego/v4/cmd/lego@latest
 sudo lego --email="user@example.com" --domains="domain.com" --http run
 sudo lego --email="user@example.com" --domains="domain.com" --http run
 sudo lego --email="user@example.com" --domains="domain.com" --http run
-sudo chown -R user:user .lego`
+sudo chown -R user:user .lego
+`
